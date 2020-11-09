1f36c7a46133f50b1438aab92e8d6457ee2cd6ff
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-11-09 09:57:27 -0500

    cambios

--- src/app/station-admin/station-admin-receivable/receivable-add/receivable-add.component.spec.ts
+++ src/app/station-admin/station-admin-receivable/receivable-add/receivable-add.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ReceivableAddComponent } from './receivable-add.component';
 
 describe('ReceivableAddComponent', () => {
--- src/app/station-admin/station-admin-receivable/receivable-add/receivable-add.component.ts
+++ src/app/station-admin/station-admin-receivable/receivable-add/receivable-add.component.ts
@@ -1,8 +1,8 @@
-import { NominaService } from './../../../services/nomina.service';
 import { EntClient } from './../../../Class/EntClient';
 import { Component, OnInit } from '@angular/core';
 import { CarteraService } from '../../../services/cartera.service';
 import { StorageService } from '../../../services/storage.service';
+import { EntConsumptionClient } from '../../../Class/EntConsumptionClient';
 
 @Component({
   selector: 'app-receivable-add',
@@ -10,31 +10,50 @@ import { StorageService } from '../../../services/storage.service';
   styleUrls: ['./receivable-add.component.css']
 })
 export class ReceivableAddComponent implements OnInit {
-    cliente: EntClient;
-    fechaIni: Date;
-    fechaFin: Date;
+    cliente: EntClient = new EntClient();
+    fechaIni: string;
+    fechaFin: string;
     cars: any[];
     virtualCars: any[];
     cols: any[];
     estacion: number;
+    consumosAll: EntConsumptionClient[]=[]
   constructor(
-      public carteraService = CarteraService,
-      public storageService = StorageService
+      public carteraService : CarteraService,
+      public storageService : StorageService
 
   ) {
     this.cols = [
         {field: 'id', header: 'id'},
-        {field: 'Ticket', header: 'Ticket'},
-        {field: 'fecha', header: 'Fecha'},
-        {field: 'Hora', header: 'Hora'}
+        // {field: 'fechaConsumo', header: 'Fecha'},
+        // {field: 'horaConsumo', header: 'Hora'},
+        {field: 'ConsecutivoEstacion', header: 'Ticket'},
+        {field: 'placa', header: 'Placa'},
+        {field: 'combustible', header: 'Combustible'},
+        {field: 'cantidad', header: 'Cantidad'},
+        {field: 'valor', header: 'Valor'},
     ];
     this.estacion = this.storageService.getCurrentStation();
+    this.ConsultaMock();
   }
 
   ngOnInit(): void {
+     this.consultarConsumos()
   }
  consultarConsumos() {
+    this.consumosAll=[];
+    this.carteraService.getConsumption(this.cliente.codCliente,this.fechaIni,this.fechaFin,null, this.estacion)
+    .subscribe(consumos =>{
+        this.consumosAll = consumos;
+        console.log(consumos);
 
-    this.carteraService.getConsumption(this.cliente.codCliente,this.fechaIni,this.fechaFin,null, this.estacion).suscribe
+    })
+ }
+
+ ConsultaMock() {
+    this.cliente.codCliente=860002566;
+    this.fechaIni='2020-10-01'; //new Date(2020,10,1);
+    this.fechaFin='2020-10-02'; //new Date(2020,10,5);
+    this.estacion=96
  }
 }
