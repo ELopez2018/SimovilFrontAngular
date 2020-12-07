ba30bc628e6df5fd07054a33d2828180a8438336
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-12-07 15:59:24 -0500

    Cambios en Factura y Documentacion

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "level": "error",
+  "name": "Movilgas",
+  "version": "1.0.0",
+  "description": "Movilgas",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-12-04T19:10:51.820Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
